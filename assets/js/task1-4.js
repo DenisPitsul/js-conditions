edc5de7130934c91e68ca733c22359cbfc84a533
DenisPitsul <denia.pitsul@gmail.com>
2024-05-16 11:07:47 +0300

    Refactored task 1.4

--- assets/js/task1-4.js
+++ assets/js/task1-4.js
@@ -1,4 +1,12 @@
-const isLogin = confirm("Is login?");
+const isLogin = confirm('Is login?');
+let userName = '';
+let className = '';
+
+if(isLogin) {
+    userName = prompt('Input you user name', 'anonymus');
+    const gender = prompt('Input "male" if you are man, "female" if you woman, any other characters or click cancel if you do not want to answer');
+    className = gender === 'male' ? 'is-male' : gender === 'female' ? 'is-female' : 'is-unknown';
+}
 
 document.write(`
     <header class="header">
@@ -7,24 +15,12 @@ document.write(`
                 <img class="logo-image" src="./assets/images/logo.png" alt="Logo" />
             </a>
             <div class="header-right">
-`);
-
-if(!isLogin) {
-    document.write(`
-        <button class="btn signin-btn" type="button">Sign In</button>
-        <button class="btn signup-btn" type="button">Sign Up</button>
-    `);
-} else {
-    const userName = prompt('Input you user name', 'anonymus');
-    const gender = prompt('Input "male" if you are man, "female" if you woman, any other characters or click cancel if you do not want to answer');
-    const className = gender === 'male' ? 'is-male' : gender === 'female' ? 'is-female' : 'is-unknown';
-    document.write(`
-        <img class="user-img ${className}" src="./assets/images/user.png" alt="User image">
-        <span class="user-name">${userName}</span>
-    `)
-}
-
-document.write(`
+                ${isLogin 
+                    ? `<img class="user-img ${className}" src="./assets/images/user.png" alt="User image">
+                       <span class="user-name">${userName}</span>`
+                    : `<button class="btn signin-btn" type="button">Sign In</button>
+                       <button class="btn signup-btn" type="button">Sign Up</button>`
+                }
             </div>
         </div>
     </header>
